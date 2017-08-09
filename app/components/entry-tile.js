867ccdc6e05e7c57c45c2edc951e5b59cedf6767
Laura Hamilton and Michael Dunlap <laura@>
2017-08-09 11:10:43 -0700

    add delete functionality for blog entries

--- app/components/entry-tile.js
+++ app/components/entry-tile.js
@@ -8,6 +8,11 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    delete (entry) {
+      if (confirm('Are you sure you want to delete this wonderment of blog entries?')) {
+        this.sendAction('destroyEntry', entry);
+      }
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('entry');
   },
+
+  actions: {
+    destroyEntry(entry) {
+      entry.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
